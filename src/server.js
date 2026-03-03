869a2c4dfe115c6227f212387160fc81ddd4d5cc
jdikseeka <dikseeka@hummingbirdindia.com>
2026-03-03 10:55:44 +0530

    Change server port from 3000 to 3002

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const financeRoutes = require('./routes/financeRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors());
